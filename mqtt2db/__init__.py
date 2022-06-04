75fb96616fd1ae219a933cb64080c7d6b211f50c
0xMRTT <0xMRTT@tuta.io>
2022-06-04 21:30:20 +0200

    Format project with black

--- mqtt2db/__init__.py
+++ mqtt2db/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Mqtt2DB."""
 
 __author__ = """0xMRTT"""
-__email__ = '0xMRTT@tuta.io'
-__version__ = '0.1.0'
+__email__ = "0xMRTT@tuta.io"
+__version__ = "0.1.0"
--- mqtt2db/cli.py
+++ mqtt2db/cli.py
@@ -6,8 +6,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for mqtt2db."""
-    click.echo("Replace this message by putting your code into "
-               "mqtt2db.cli.main")
+    click.echo("Replace this message by putting your code into " "mqtt2db.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 

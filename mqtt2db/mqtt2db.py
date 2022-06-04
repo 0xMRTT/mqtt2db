efc67f1ec8252b45ecf051b0bbfdd22173ec15fd
0xMRTT <0xMRTT@tuta.io>
2022-06-04 22:24:40 +0200

    Add config func

--- mqtt2db/mqtt2db.py
+++ mqtt2db/mqtt2db.py
@@ -1,1 +1,28 @@
 """Main module."""
+
+from pathlib import Path
+MQTT2DB_CONFIG_FILE: Path = Path("~/.config/mqtt2db.toml").expanduser()
+import toml
+import click
+
+def config_set(key:str, value:str) -> None:
+    with MQTT2DB_CONFIG_FILE.open("a") as config_file:
+        config_dict:dict = toml.load(config_file)
+        try:
+            config_dict[key] = value
+        except Exception as e:
+            return False
+        else:
+            toml.dump(config_dict, config_file)
+            return True
+
+
+def config_get(key:str) -> None:
+    with MQTT2DB_CONFIG_FILE.open("r") as config_file:
+        config_dict:dict = toml.load(config_file)
+        try:
+            get = config_dict[key]
+        except Exception as e:
+            return False
+        else:
+            return get

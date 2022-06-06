9e728a91e35fc4e6f3744bb0956aae06f65195b1
0xMRTT <0xMRTT@tuta.io>
2022-06-06 16:56:15 +0200

    Remove logging and format

--- mqtt2db/mqtt2db.py
+++ mqtt2db/mqtt2db.py
@@ -1,24 +1,26 @@
 """Main module."""
 
+import logging
 from pathlib import Path
 
-MQTT2DB_CONFIG_FILE = Path("~/.config/mqtt2db.toml").expanduser()
 import toml
-import logging
+
+MQTT2DB_CONFIG_FILE = Path("~/.config/mqtt2db.toml").expanduser()
 
 logger = logging.getLogger("mqtt2db")
 
 
 def config_get(key: str) -> None:
     """Get value from the config file"""
-    with MQTT2DB_CONFIG_FILE.open("r") as config_file: # Open the config file
-        logger.info(f"Open {MQTT2DB_CONFIG_FILE}")
-        config_dict: dict = toml.load(config_file) # Load the file and parse the content with toml
-        try: # Try to get the key
+    with MQTT2DB_CONFIG_FILE.open(
+        "r", encoding="UTF-8"
+    ) as config_file:  # Open the config file
+        config_dict: dict = toml.load(
+            config_file
+        )  # Load the file and parse the content with toml
+        try:  # Try to get the key
             get = config_dict[key]
-        except KeyError as e: # If there is no key return False and log the error
-            logger.error(f"Unable to found key {e}")
+        except KeyError:  # If there is no key return False
             return False
-        else: # Else, return the value and log the value
-            logger.info(f"{key} -> {get}")
+        else:  # Else, return the value
             return get

f3f5e5549af0b9a4229041d20e802847ce6c5869
0xMRTT <0xMRTT@tuta.io>
2022-06-06 17:04:41 +0200

    Add except

--- mqtt2db/mqtt2db.py
+++ mqtt2db/mqtt2db.py
@@ -16,8 +16,19 @@ with MQTT2DB_CONFIG_FILE.open(
         config_file
     )  # Load the file and parse the content with toml
 
-LOG_LEVEL = CONFIG["logging"]["level"]
-LOG_PATH = CONFIG["logging"]["path"]
-LOG_FILEMOD = CONFIG["logging"]["filemod"]
-
-logging.basicConfig(filename=LOG_PATH, filemode=LOG_FILEMOD, level=LOG_LEVEL)
+try:
+    LOG_LEVEL = CONFIG["logging"]["level"]  # Get the log level from the config
+    LOG_PATH = CONFIG["logging"]["path"]  # Get the log path from the config
+    LOG_FILEMOD = CONFIG["logging"]["filemod"]  # Get the log filemod from the config
+except KeyError:
+    logging.basicConfig(filename="mqtt2db.log", level=logging.INFO, filemode="w")
+    logger = logging.getLogger("mqtt2db")
+    logger.warning(
+        "No logging configuration found in config file. Using default logging configuration."
+    )
+else:
+    logging.basicConfig(filename=LOG_PATH, filemode=LOG_FILEMOD, level=LOG_LEVEL)
+    logger = logging.getLogger("mqtt2db")
+    logger.info("Logging configuration loaded from config file.")
+finally:
+    logger.info("Logging configuration loaded.")
